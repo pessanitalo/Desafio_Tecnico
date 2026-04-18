9ad9bb6750dae9e87ac722f19fbbbea14a47d237
Italo Pessan <italopessan@gmail.com>
2026-04-18 16:55:49 -0300

    classe responsavel por retornar o acesstoken e refresh token

---
+++ Desafio_Tecnico/Desafio_Tecnico.Application/DTOs/LoginResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Desafio_Tecnico.Application.DTOs
+{
+    public class LoginResponse
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+
+        public LoginResponse(string accessToken, string refreshToken)
+        {
+            AccessToken = accessToken;
+            RefreshToken = refreshToken;
+        }
+    }
+}
