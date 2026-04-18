2b2fc8dfa842e392a24aab0b239a14d898707943
Italo Pessan <italopessan@gmail.com>
2026-04-18 16:21:40 -0300

    controller para fins de testes necessário para a validação que valida que a api esta respondendo ok no ambiente aws

---
+++ Desafio_Tecnico/Desafio_Tecnico.Api/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Desafio_Tecnico.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Check()
+        {
+            return Ok(new { status = "OK", message = "API de Consulta está funcionando!" });
+        }
+
+    }
+}
