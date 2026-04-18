4d65d97d3e5ee8374c73b96adac773d98e6742dd
Italo Pessan <italopessan@gmail.com>
2026-04-18 16:50:47 -0300

    classe para deixar a injeção de dependencia mais limpa

---
+++ Desafio_Tecnico/Desafio_Tecnico.Api/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Desafio_Tecnico.Application.Consulta.UseCases;
+using Desafio_Tecnico.Application.Login.UseCases;
+using Desafio_Tecnico.Application.Paciente.UseCases;
+using Desafio_Tecnico.Application.Profissional.UseCases;
+using Desafio_Tecnico.Application.Usuario.UseCases;
+using Desafio_Tecnico.Domain.Repositories;
+using Desafio_Tecnico.Domain.Service;
+using Desafio_Tecnico.Infrastructure.Persistence.Repository;
+using Desafio_Tecnico.Infrastructure.Services;
+
+namespace Desafio_Tecnico.Api.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationservices(this IServiceCollection services)
+        {
+            services.AddScoped<ICreatePacienteUseCase, CreatePacienteUseCase>();
+            services.AddScoped<ICreateProfissionalUseCase, CreateProfissionalUseCase>();
+            services.AddScoped<ICreateConsultaUseCase, CreateConsultaUseCase>();
+
+            services.AddScoped<IConsultaRepository, ConsultaRepository>();
+            services.AddScoped<IPacienteRepository, PacienteRepository>();
+            services.AddScoped<IProfissionalRepository, ProfissionalRepository>();
+
+            services.AddScoped<ICreateUsuarioUseCase, CreateUsuarioUseCase>();
+            services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+
+            services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+            services.AddScoped<IGetAllPacienteUseCase, GetAllPacienteUseCase>();
+
+            services.AddScoped<IGetAllProfissionalUseCase, GetAllProfissionalUseCase>();
+            services.AddScoped<IObterDetalheConsultaUseCase, ObterDetalheConsultaUseCase>();
+            services.AddScoped<IObterTodasConsultasUseCase, ObterTodasConsultasUseCase>();
+            services.AddScoped<IPesquisarPacientePorCpfUseCase, PesquisarPacientePorCpfUseCase>();
+
+            services.AddScoped<IObterProfissionalUseCase, ObterProfissionalUseCase>();
+            services.AddScoped<IObterProfissionalUseCase, ObterProfissionalUseCase>();
+
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPasswordHasherService, PasswordHasherService>();
+            services.AddScoped<IObterAgendaPorProfissionalIdUseCase, ObterAgendaPorProfissionalIdUseCase>();
+
+            services.AddScoped<ILoginUseCase, LoginUseCase>();
+
+            return services;
+        }
+    }
+}
