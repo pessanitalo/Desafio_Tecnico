640414b52275fcc06b671c2190c9aea36dcdf49f
Italo Pessan <italopessan@gmail.com>
2026-04-17 23:13:53 -0300

    componente responsavel por renderizar o formulario de criação de uma consulta

---
+++ Desafio_Tecnico.App/src/app/consultation/models/consultation-create.ts
@@ -0,0 +1,6 @@
+export interface ConsultationCreate {
+  pacienteId: number;
+  profissionalId: number;
+  dataConsulta: string;
+  horaConsulta: string;
+}
---
+++ Desafio_Tecnico.App/src/app/consultation/models/consultation.ts
@@ -0,0 +1,8 @@
+export interface Consultation {
+  id: number;
+  profissionalId: number;
+  pacienteNome: string;
+  profissionalNome: string;
+  dataConsulta: string;
+  horaConsulta: string;
+}
