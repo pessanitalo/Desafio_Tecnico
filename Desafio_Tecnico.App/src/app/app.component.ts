93e93d8252ea65cd0b73c6f1785b6db592f17850
Italo Pessan <italopessan@gmail.com>
2026-04-19 20:59:57 -0300

    arquivos de configurações do angular

--- Desafio_Tecnico.App/src/app/app.component.ts
+++ Desafio_Tecnico.App/src/app/app.component.ts
@@ -1,14 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
 import { MenuComponent } from './navegacao/menu/menu.component';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponent, RouterLink],
+  imports: [CommonModule, RouterOutlet, MenuComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Desafio_Tecnico.App';
+  showMenu = true;
+
+  constructor(private router: Router) {
+    this.updateMenuVisibility(this.router.url);
+
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        this.updateMenuVisibility((event as NavigationEnd).urlAfterRedirects);
+      });
+  }
+
+  private updateMenuVisibility(url: string) {
+    this.showMenu = !['/login', '/user-novo'].includes(url);
+  }
 }
