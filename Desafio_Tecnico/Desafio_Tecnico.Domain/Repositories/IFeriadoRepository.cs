6ff35a1f130af700280797324dda0cf93e2d38c9
Italo Pessan <italopessan@gmail.com>
2026-04-20 08:50:41 -0300

    repositorio do feriado

---
+++ Desafio_Tecnico/Desafio_Tecnico.Domain/Repositories/IFeriadoRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Desafio_Tecnico.Domain.Repositories
+{
+    public interface IFeriadoRepository
+    {
+        Task<bool> EhFeriadoAsync(DateTime data);
+    }
+}
---
+++ Desafio_Tecnico/Desafio_Tecnico.Infrastructure/Persistence/Repository/FeriadoRepository.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using Desafio_Tecnico.Domain.Repositories;
+using System.Data;
+
+
+namespace Desafio_Tecnico.Infrastructure.Persistence.Repository
+{
+    public class FeriadoRepository : IFeriadoRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public FeriadoRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task<bool> EhFeriadoAsync(DateTime data)
+        {
+            const string sql = "SELECT 1 FROM Feriado WHERE Data = @Data";
+            var resultado = await _connection.QueryFirstOrDefaultAsync(sql, new { Data = data.Date });
+             return resultado != null;
+        }
+    }
+}
