620cc310d64f7af39195e6467045e13f262a0c36
Italo Pessan <italopessan@gmail.com>
2026-04-20 08:50:12 -0300

    classe auxiliar para verificar os feriados disponiveis

---
+++ Desafio_Tecnico/Desafio_Tecnico.Domain/Entities/Feriado.cs
@@ -0,0 +1,9 @@
+﻿namespace Desafio_Tecnico.Domain.Entities
+{
+    public class Feriado
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public string Descricao { get; set; }
+    }
+}
