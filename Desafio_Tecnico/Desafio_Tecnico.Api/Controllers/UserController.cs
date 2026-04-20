5c05f5fcb8ea6a89dd973ce3f8be8c3200ee0c66
Italo Pessan <italopessan@gmail.com>
2026-04-20 08:55:01 -0300

    controller do usuario

---
+++ Desafio_Tecnico/Desafio_Tecnico.Api/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Desafio_Tecnico.Application.Usuario.DTOs;
+using Desafio_Tecnico.Application.Usuario.UseCases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Desafio_Tecnico.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ICreateUsuarioUseCase _usuarioService;
+ 
+        public UserController(ICreateUsuarioUseCase emprestimoService)
+        {
+            _usuarioService = emprestimoService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUsuarioDTO usuarioDTO)
+        {
+
+            var resultado = await _usuarioService.AddAsync(usuarioDTO);
+
+            if (!resultado.Success)
+                return BadRequest(new { errors = new[] { resultado.Error } });
+            return Created(string.Empty, new { data = resultado.Data });
+
+        }
+    }
+}
