c580e501f6bf3eddd373971c83c921850c36b58e
Italo Pessan <italopessan@gmail.com>
2026-05-07 11:51:08 -0300

    atualização dos pacotes angular

--- Desafio_Tecnico.App/src/app/app.config.ts
+++ Desafio_Tecnico.App/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { IConfig, provideNgxMask } from 'ngx-mask'
 
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import {  provideHttpClient } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 
